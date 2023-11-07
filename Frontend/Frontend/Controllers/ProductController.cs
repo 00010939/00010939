31c80b2944409676ed457eb2039ce40633cda39c
00010939 <91649111+00010939@users.noreply.github.com>
2023-11-07 17:41:42 +0500

    Add files via upload
    get data and configuration

---
+++ Frontend/Frontend/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using Frontend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Frontend.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public async Task<ActionResult> Index()
+        {
+            string Baseurl = "https://localhost:44350/";
+            List<Product> ProdInfo = new List<Product>();
+            using (var client = new HttpClient()) ;
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+
+                client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                HttpResponseMessage Res = await client.GetAsync("api/Products");
+
+                if (Res.IsSuccessStatusCode)
+                {  
+
+                    var PrResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    ProdInfo = JsonConvert.DeserializeObject<List<Product>>(PrResponse);
+
+                }
+
+                return View(ProdInfo);
+            }
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
